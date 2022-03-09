2c65e51ab44038f196958e3c3801114d92233438
Alex <>
2022-03-09 21:22:05 +0100

    web scraping works.

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
-	"io/ioutil"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func main() {
-	response, err := http.Get("https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA")
-	if err != nil {
-		log.Print(err)
-	}
-	responseBody, _ := ioutil.ReadAll(response.Body)
+// func getResponseString()
 
-	log.Print(response.Status)
-	log.Print(string(responseBody))
+func main() {
+	res, err := http.Get("https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA")
+	// res, err := http.Get("http://metalsucks.net")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// load HTML doc
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("link").Each(func(i int, s *goquery.Selection) {
+		link, _ := s.Attr("href")
+		fmt.Println("item ", i, link)
+
+	})
 
 }
